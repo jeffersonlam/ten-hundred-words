07ef7bbb8005f8f0aa628d027b6938d35a0fe21c
Jefferson Lam <jeffersonwlam@gmail.com>
2015-08-02 12:32:03 -0500

    Initial commit with gulp starter
    Add data.json containing most common 10,000 words

---
+++ README.md
@@ -0,0 +1,30 @@
+# gulp-starter
+
+This is a starter repo with Gulp. Gulp tasks include:
+- Compiling Sass
+- CSS and JS Minification
+- Browsersync
+- Sass lint
+- JS hint
+
+## Getting started
+You will need:
+- Node
+- Npm
+- Gulp  
+
+
+##### 1. Clone this repo
+```bash
+git clone https://github.com/jeffersonlam/gulp-starter.git
+```
+##### 2. In the cloned folder, install the dependencies
+```bash
+cd gulp-starter
+npm install
+```
+##### 3. Start up gulp
+```bash
+gulp
+```
+##### 4. Write some code!
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var scsslint = require('gulp-scss-lint');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('scss', function() {
+  return gulp.src('scss/main.scss')
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(reload({stream:true}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
+});
+
+gulp.task('deploy', function () {
+    return gulp.src('dist/**/*')
+        .pipe(deploy());
+});
+
+gulp.task('js', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(reload({stream:true}))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('scss-lint', function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(scsslint());
+});
+
+gulp.task('html', function() {
+  gulp.src('./*.html')
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('jshint', function() {
+  gulp.src('js/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['scss']);
+  gulp.watch('js/*.js', ['jshint', 'js']);
+  gulp.watch('./*.html', ['html']);
+});
+
+gulp.task('default', ['scss-lint', 'browser-sync', 'js', 'html', 'scss', 'watch']);
